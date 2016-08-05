df5965479d7d148d2b3f3bea88828a2ed9b5ecbc
NIK2 <Email@qq.com>
2016-08-06 00:01:13 +0800

    【ADD】添加双向链表节点类

---
+++ DataStructure/Chapter2/DbNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Chapter2
+{
+    /// <summary>
+    /// 双向链表节点
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class DbNode<T>
+    {
+    }
+}
