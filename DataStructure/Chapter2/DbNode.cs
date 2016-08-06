b591ad08cef2e044305efdff47a65ad5db5978a4
Nik <kai364@qq.com>
2016-08-06 10:05:38 +0800

    【EDIT】完成DBNode

--- DataStructure/Chapter2/DbNode.cs
+++ DataStructure/Chapter2/DbNode.cs
@@ -12,5 +12,48 @@ namespace DataStructure.Chapter2
     /// <typeparam name="T"></typeparam>
     class DbNode<T>
     {
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// 前驱
+        /// </summary>
+        public DbNode<T> Prev { get; set; }
+
+        /// <summary>
+        /// 后继
+        /// </summary>
+        public DbNode<T> Next { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public DbNode()
+        {
+            Data = default(T);
+            Next = null;
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="data">节点数据</param>
+        public DbNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="next">后继</param>
+        public DbNode(DbNode<T> next)
+        {
+            Data = default(T);
+            Next = next;
+        }
     }
 }
