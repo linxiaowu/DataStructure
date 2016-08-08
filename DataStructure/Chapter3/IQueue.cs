228272aebe7749191818ea5f500445b6ac9281b9
Nik <kai364@qq.com>
2016-08-08 16:04:27 +0800

    【ADD】添加队列接口

---
+++ DataStructure/Chapter3/IQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Chapter3
+{
+    /// <summary>
+    /// 队列接口
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    interface IQueue<T>
+    {
+        /// <summary>
+        /// 获取队列长度
+        /// </summary>
+        /// <returns></returns>
+        int GetLength();
+
+        /// <summary>
+        /// 判断队列是否为空
+        /// </summary>
+        /// <returns></returns>
+        bool IsEmpty();
+
+        /// <summary>
+        /// 清空队列
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// 入队
+        /// </summary>
+        /// <param name="item"></param>
+        void In(T item);
+
+        /// <summary>
+        /// 出队
+        /// </summary>
+        /// <returns></returns>
+        T Out();
+
+        /// <summary>
+        /// 或取队头元素
+        /// </summary>
+        /// <returns></returns>
+        T GetFront();
+    }
+}
